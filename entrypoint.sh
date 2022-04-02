1448403671c14b79afd7bc6698336bbb759e62da
Adam Magnier <github@magnier.io>
2022-04-02 11:11:45 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh -l
 
+namespace="$(grep namespace: galaxy.yml | cut -d ' ' -f 2)"
+name="$(grep name: galaxy.yml | cut -d ' ' -f 2)"
+version="$(grep version: galaxy.yml | cut -d ' ' -f 2)"
+
+tarball="/github/workspace/$namespace-$name-$version.tar.gz"
+
 ansible-galaxy collection build --token $1 --server $2
-ansible-galaxy collection publish --token $1  --server $2 ./pkg.tar.gz
+ansible-galaxy collection publish --token $1  --server $2 $tarball
